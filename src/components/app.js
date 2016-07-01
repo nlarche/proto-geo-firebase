3aee947505b6b0515c6f839946a7b5be8a819f32
nilar <nicolas.larche@rca.fr>
2016-07-01 10:15:18 +0200

    add firebase to component

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,95 @@
 import React from 'react';
 
-const App = React.createClass({
-    getInitialState() {
-        return {
-            text: ''
+import firebase from 'firebase';
+import GeoFire from 'geofire';
+
+import init from '../initData.js';
+import currentPosition from '../currentPosition.js';
+import geocoder from '../geocoder.js';
+
+import { FIREBASE_API_KEY, FIREBASE_URL } from '../configuration.js';
+
+// Initialize the Firebase SDK
+firebase.initializeApp({
+    apiKey: FIREBASE_API_KEY,
+    databaseURL: FIREBASE_URL,
+});
+
+// Generate a random Firebase location
+const firebaseRef = firebase.database().ref();
+
+// Create a new GeoFire instance at the random Firebase location
+const geoFire = new GeoFire(firebaseRef.child("_geofire"));
+let geoQuery;
+
+init(firebaseRef, geoFire);
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
         };
-    },
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleResult = this.handleResult.bind(this);          
+    }
+    handleResult(key, location, distance) {
+
+        const data = this.state.data;
+
+        firebaseRef.child("locations").child(key).once("value", (dataSnapshot) => {
+
+            const location = dataSnapshot.val();        
+
+            data.push(location);
+
+            console.log(location);
+
+            this.setState({ data: data });
+
+        });
+    }
     handleSubmit(e) {
         e.preventDefault();
-        const address = this.refs.address.value;
-        this.refs.adressForm.reset();
-        this.refs.address.focus();
+        const text = this.refs.input.value;
+        this.refs.form.reset();
+        this.refs.input.focus();
 
-        this.setState({
-            text: address
+        geocoder(text, (coords) => {
+
+            geoQuery.updateCriteria({
+                center: [coords[0].latitude, coords[0].longitude],
+                radius: 5
+            });
         });
-    },
+    }
+    componentDidMount() {
+
+        const handleResult = this.handleResult;
+
+        currentPosition((position) => {
+
+            geoQuery = geoFire.query({
+                center: [position.latitude, position.longitude],
+                radius: 2
+            });
+
+            geoQuery.on("key_entered", handleResult);
+        });
+    }
+
     render() {
         return (
             <div>
-                <form ref="adressForm" onSubmit={ this.handleSubmit }>
-                    <input type="text" ref="address" placeholder="address" />
+                <form ref="form" onSubmit={ this.handleSubmit }>
+                    <input type="text" ref="input" placeholder="address" />
                 </form>
-                { this.state.text }
-            </div>
+                { this.state.data.map((d) => {
+                    <span>d.info.address</span>;
+                }) }
+            </div >
         );
     }
-});
+}
 
 export default App;
